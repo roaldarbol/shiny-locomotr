b7693ce661637b3c65a3c7a4fd52d24bbff9732a
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2022-06-07 23:44:13 +0200

    use shinydashboard

--- shiny-locomotr/server.R
+++ shiny-locomotr/server.R
@@ -15,134 +15,105 @@ library(plotly)
 library(RColorBrewer)
 library(vroom)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# Define server logic
+shinyServer(function(input, output){
+  output$mocapUploaded <- reactive({
+      if(!is.null(input$mocap)) return(TRUE)
+  })
+  
+  output$varsSelected <- reactive({
+      if(!is.null(input$xvariable)) return(TRUE)
+  })
+  
+  outputOptions(output, 'mocapUploaded', suspendWhenHidden=FALSE)
+  outputOptions(output, 'varsSelected', suspendWhenHidden=FALSE)
+  
+  
+  # UI Output ----
+  output$bodypart <- renderUI({
+      selectInput('bodypart', 'Select bodypart', bodyparts())
+  })
+  
+  output$frame <- renderUI({
+    frames <- frames()
+    frames_min <- min(frames)
+    frames_max <- max(frames)
+    sliderInput("frame", "Frames:",
+                min = frames_min, max = frames_max,
+                value = c(frames_min,frames_max))
+  })
+  
+  output$boxy <- renderValueBox({
+    valueBox(
+      paste0(25 + input$frame[2], "%"), "Progress", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  # Variable input
+  mocap_raw <- reactive({
+    data_raw <- suppressMessages(vroom::vroom(Sys.glob(input$mocap$datapath)))
+    return(data_raw)
+  })
+  
+  mocap_data <- reactive({
+    data_tidy <- tidy_anipose(mocap_raw())
+    data_augmented <- augment_poses(data_tidy, 30, 30)
+    return(data_augmented)
+  })
+  
+  mocap_data_filtered <- reactive({
+    data <- mocap_data()
+    frame_min <- input$frame[1]
+    frame_max <- input$frame[2]
+    data_filtered <- data %>% 
+      filter(between(frame, frame_min, frame_max))
+    return(data_filtered)
+  })      
     
-    output$mocapUploaded <- reactive({
-        if(!is.null(input$mocap)) return(TRUE)
-    })
-    
-    output$varsSelected <- reactive({
-        if(!is.null(input$xvariable)) return(TRUE)
-    })
-    
-    outputOptions(output, 'mocapUploaded', suspendWhenHidden=FALSE)
-    outputOptions(output, 'varsSelected', suspendWhenHidden=FALSE)
-    
-    # Variable input
-    # outSheet <- reactive({
-    #     mydata <- mocapWrangled()
-    #     if (class(mydata) == 'list'){
-    #         names <- names(data.list)
-    #     } else {
-    #         names = NULL
-    #     }
-    #     return(names)
-    # })
-    
-    outNames <- reactive({
-        # if (length(mydata) == 1){
-        #     mydata <- mydata[[1]]
-        mydata <- mocap_data[-1]
-        names <- names(mydata)
-        # } else if (class(mydata) == 'list'){
-        #     names <- names(mydata[outSheet])
-        # }
-        return(names)
-    })
-    
-    bodyparts <- reactive({
-      data <- mocap_data()
-      bodyparts <- unique(data$bodypart)
-      return(bodyparts)
-    })
-    
-    frames <- reactive({
-      data <- mocap_data()
-      frames <- unique(data$frame)
-      return(frames)
-    })
-    
-    
-    # output$xvar <- renderUI({
-    #     selectInput('xvar', 'Select X Variable', outNames(), selected = 'Min')
-    # })
-    # 
-    output$bodypart <- renderUI({
-        selectInput('bodypart', 'Select bodypart', bodyparts())
-    })
-    
-    output$frame <- renderUI({
-      frames <- frames()
-      frames_min <- min(frames)
-      frames_max <- max(frames)
-      sliderInput("frame", "Frames:",
-                  min = frames_min, max = frames_max,
-                  value = c(frames_min,frames_max))
-    })
-    
-    # output$sheet <- renderUI({
-    #     selectInput('sheet', 'Select Data Sheet', outSheet(), selected = 'Overall')
-    # })
-    
-    mocap_raw <- reactive({
-        data_raw <- suppressMessages(vroom::vroom(Sys.glob(input$mocap$datapath)))
-        return(data_raw)
-    })
-    
-    mocap_data <- reactive({
-      data_tidy <- tidy_anipose(mocap_raw())
-      data_augmented <- augment_poses(data_tidy, 30, 30)
-      return(data_augmented)
-    })
-    
-    mocap_data_filtered <- reactive({
-      data <- mocap_data()
-      frame_min <- input$frame[1]
-      frame_max <- input$frame[2]
-      data_filtered <- data %>% 
-        filter(between(frame, frame_min, frame_max))
-      return(data_filtered)
-    })
-    
-    # mocap Scatterplot
-    output$mocap_line <- renderPlotly({
-        # x <- input$xvar
-        # y <- input$yvar
-        # if (is.null(input$sheet)){
-        #     df <- mocapWrangled()[[1]]
-        # } else if (!is.null(input$sheet)){
-        #     df <- mocapWrangled()[[input$sheet]]
-        # }
-        plot <- mocap_data_filtered() %>%
-          filter(bodypart == input$bodypart) %>%
-          ggplot(aes(frame, v)) +
-          geom_line()
-        ggplotly(plot)
-    })
-    
-    output$mocap_pose <- renderPlotly({
-      mocap_data_filtered()  %>%
-        plot_ly(
-          x = ~x,
-          y = ~y,
-          frame = ~frame,
-          color = ~bodypart,
-          type = 'scatter',
-          mode = 'markers',
-          showlegend = TRUE
-        ) %>%
-        layout(
-          yaxis = list(
-            scaleanchor = "x",
-            scaleratio = 1
-          )
-        ) %>% 
-        animation_opts(
-          frame = 10,
-          transition = 0
+  bodyparts <- reactive({
+    data <- mocap_data()
+    bodyparts <- unique(data$bodypart)
+    return(bodyparts)
+  })
+  
+  frames <- reactive({
+    data <- mocap_data()
+    frames <- unique(data$frame)
+    return(frames)
+  })
+  
+  
+  # Plots ----
+  output$mocap_line <- renderPlotly({
+      plot <- mocap_data_filtered() %>%
+        filter(bodypart == input$bodypart) %>%
+        ggplot(aes(frame, v)) +
+        geom_line()
+      ggplotly(plot)
+  })
+  
+  output$mocap_pose <- renderPlotly({
+    mocap_data_filtered()  %>%
+      plot_ly(
+        x = ~x,
+        y = ~y,
+        frame = ~frame,
+        color = ~bodypart,
+        type = 'scatter',
+        mode = 'markers',
+        showlegend = TRUE
+      ) %>%
+      layout(
+        yaxis = list(
+          scaleanchor = "x",
+          scaleratio = 1
         )
-    })
-    
-    
+      ) %>% 
+      animation_opts(
+        frame = 10,
+        transition = 0
+      )
+  })
+  
 })
--- shiny-locomotr/ui.R
+++ shiny-locomotr/ui.R
@@ -14,58 +14,162 @@ library(plotly)
 library(shinyWidgets)
 library(RColorBrewer)
 library(png)
+library(shinydashboard)
+library(dashboardthemes)
+library(tidymocap)
+library(locomotr)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    # Application title
-  setBackgroundColor("#475cc7"),
-    # setBackgroundImage(src = "dansk-atletik.jpeg"), 
-    titlePanel(h1(img(height = 1)), windowTitle = 'Analytics'),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            titlePanel(h1(img(width = 200, src="locomotr-transparent.png"), align="center")),
-            # titlePanel(h1('mocap Analytics', align="center")),
-            # passwordInput('passwd', label='Enter Password'),
-            # conditionalPanel(condition= "input.passwd == 'mocap'",
-            #                  
-                             conditionalPanel(condition="output.mocapUploaded",
-                                              # conditionalPanel(condition="!is.null(output.outSheet)",
-                                              #                  uiOutput('sheet')),
-                                              uiOutput('bodypart'),
-                                              uiOutput('frame')
-                                              # uiOutput('xvar')
-                                              ),
-            
-                             fileInput("mocap", "Select Mocap Data",
-                                       multiple = FALSE,
-                                       accept = c(
-                                           ".csv")
-                                       ),
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            tabsetPanel(
-              tabPanel("Graphs",
-                       fluidRow(
-                         conditionalPanel(condition="output.mocapUploaded",
-                                          plotlyOutput("mocap_line")
-                         )
-                       )
-              ),
-              
-              tabPanel("Animation",
-                       fluidRow(
-                           conditionalPanel(condition="output.mocapUploaded",
-                                            plotlyOutput("mocap_pose"))
-
-                       )
-              ),
-              
-              tabPanel("Modelling")
-            )
-        )
+# Dashboard page ----
+dashboardPage(
+  
+  # Header ----
+  dashboardHeader(
+    title = "Performance Analysis",
+    tags$li(a(href = 'https://roald-arboel.com',
+              img(src = 'locomotr-transparent.png',
+                  title = "Company Home", height = "30px"),
+              style = "padding-top:10px; padding-bottom:10px;"),
+            class = "dropdown")
+      # img(src = "locomotr-transparent.png", height=50)#",
+  ),
+  
+  # Sidebar ----
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Welcome", tabName = "welcome", icon = icon("welcome")),
+      menuItem("New analysis", tabName = "dashboard", icon = icon("dashboard"), startExpanded = FALSE,
+               menuSubItem("Import", tabName = "import", icon = icon("download")),
+               menuSubItem("Analysis", tabName = "analysis", icon = icon("dashboard")),
+               menuSubItem("Export", tabName = "export", icon = icon("upload"))),
+      menuItem("Progression", icon = icon("calendar"), tabName = "progression")
+      # titlePanel(h1(img(width = 200, src="locomotr-transparent.png"), align="center")),
     )
-))
+  ),
+  
+  # Body ----
+  dashboardBody(
+    shinyDashboardThemes(
+      theme = "blue_gradient"
+    ),
+    
+    tabItems(
+      ## Tab: New Analysis ----
+      tabItem(tabName = "dashboard",
+        fluidRow(
+          valueBoxOutput("boxy")
+        )
+      ),
+      
+      ## Tab: Progression ----
+      tabItem(
+        tabName = "import",
+        fluidRow(
+          box(
+            selectInput(
+              'event',
+              'Select event',
+              c('Discus', 'Hammer', 'Shot put', 'Javelin'),
+              selected = 'Discus'
+            ),
+            
+            fileInput("mocap", "Select Mocap Data",
+                      multiple = FALSE,
+                      accept = c(
+                        ".csv")
+            )
+          )
+        )
+      ),
+      
+      ## Tab: Analysis ----
+      tabItem(
+        tabName = "analysis",
+        tabBox(
+          width = 12,
+          tabPanel(
+            "Raw data",
+            fluidRow(
+              # conditionalPanel(condition="output.mocapUploaded",
+                               uiOutput('frame'),
+                               uiOutput('bodypart')
+              # )
+            ),
+            fluidRow(
+              conditionalPanel(condition="output.mocapUploaded",
+                               plotlyOutput("mocap_line")
+              )
+            )
+          ),
+          
+          tabPanel("Animation",
+                   fluidRow(
+                     conditionalPanel(condition="output.mocapUploaded",
+                                      plotlyOutput("mocap_pose"))
+                     
+                   )
+          ),
+          
+          tabPanel("Key Performance Indicators")
+        )
+      )
+    )
+  ),
+)
+# Vanilla ----
+# Define UI for application that draws a histogram
+# shinyUI(fluidPage(
+#     # Application title
+#   # setBackgroundColor("#475cc7"),
+#     # setBackgroundImage(src = "dansk-atletik.jpeg"),
+#     titlePanel(h1(img(height = 1)), windowTitle = 'Analytics'),
+# 
+#     # Sidebar with a slider input for number of bins
+#     sidebarLayout(
+#         sidebarPanel(
+#             titlePanel(h1(img(width = 200, src="locomotr-transparent.png"), align="center")),
+#             selectInput(
+#               'event',
+#               'Select event',
+#               c('Discus', 'Hammer', 'Shot put', 'Javelin'),
+#               selected = 'Discus'
+#               ),
+#             # titlePanel(h1('mocap Analytics', align="center")),
+#             # passwordInput('passwd', label='Enter Password'),
+#             # conditionalPanel(condition= "input.passwd == 'mocap'",
+# 
+#            fileInput("mocap", "Select Mocap Data",
+#                      multiple = FALSE,
+#                      accept = c(
+#                          ".csv")
+#                      ),
+# 
+#             conditionalPanel(condition="output.mocapUploaded",
+#                              uiOutput('frame'),
+#                              uiOutput('bodypart')
+#             ),
+#         ),
+# 
+#         # Show a plot of the generated distribution
+#         mainPanel(
+#             tabsetPanel(
+#               tabPanel("Graphs",
+#                        fluidRow(
+#                          conditionalPanel(condition="output.mocapUploaded",
+#                                           plotlyOutput("mocap_line")
+#                          )
+#                        )
+#               ),
+# 
+#               tabPanel("Animation",
+#                        fluidRow(
+#                            conditionalPanel(condition="output.mocapUploaded",
+#                                             plotlyOutput("mocap_pose"))
+# 
+#                        )
+#               ),
+# 
+#               tabPanel("Key Performance Indicators")
+#             )
+#         )
+#     )
+# ))
